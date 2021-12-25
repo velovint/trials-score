56a7e18983e63e2a630631e87650d48ef4fa5882
Vitali Yakavenka <vitali.yakavenka@equest.com>
2021-12-24 17:36:15 -0800

    Bootstrap rider scores page

--- app/src/main/java/net/yakavenka/trialsscore/EventScoreFragment.kt
+++ app/src/main/java/net/yakavenka/trialsscore/EventScoreFragment.kt
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment
 
 class EventScoreFragment : Fragment() {
 
+//    private lateinit var binding: EventScoreFragmentBinding
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
