5adea1f2915fd3ae2a8da33efa68205f787f8356
Vitali Yakavenka <vitali.yakavenka@equest.com>
2021-12-24 19:35:24 -0800

    Display entry from prepopulated database

--- app/src/main/java/net/yakavenka/trialsscore/EventScoreFragment.kt
+++ app/src/main/java/net/yakavenka/trialsscore/EventScoreFragment.kt
@@ -1,16 +1,20 @@
 package net.yakavenka.trialsscore
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import net.yakavenka.trialsscore.data.RiderScoreDao
 import net.yakavenka.trialsscore.databinding.FragmentEventScoreBinding
+import net.yakavenka.trialsscore.databinding.SectionScoreItemBinding
 import net.yakavenka.trialsscore.model.RiderScoreAdapter
 
 class EventScoreFragment : Fragment() {
@@ -36,9 +40,11 @@ class EventScoreFragment : Fragment() {
 
         val adapter = RiderScoreAdapter({})
         binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this.context)
 
         scoreDao.getAll().asLiveData().observe(viewLifecycleOwner) { scores ->
-            adapter.submitList(scores)
+//            Log.d("EventScoreFragment", "Got scores " + scores)
+            scores.let { adapter.submitList(scores) }
         }
     }
 
--- app/src/main/java/net/yakavenka/trialsscore/model/RiderScoreAdapter.kt
+++ app/src/main/java/net/yakavenka/trialsscore/model/RiderScoreAdapter.kt
@@ -1,27 +1,32 @@
 package net.yakavenka.trialsscore.model
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import net.yakavenka.trialsscore.data.RiderScore
-import net.yakavenka.trialsscore.databinding.FragmentEventScoreBinding
+import net.yakavenka.trialsscore.databinding.RiderScoreItemBinding
 
 
 class RiderScoreAdapter(
     private val onClick: (EventScore) -> Unit
 ) : ListAdapter<RiderScore, RiderScoreAdapter.ViewHolder>(DIFF_CALLBACK) {
 
-    class ViewHolder(binding: FragmentEventScoreBinding) : RecyclerView.ViewHolder(binding.root) {
-
-    }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        return ViewHolder(RiderScoreItemBinding.inflate(LayoutInflater.from(parent.context)))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(getItem(position))
+    }
+
+    class ViewHolder(private val binding: RiderScoreItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(riderScore: RiderScore) {
+            binding.apply {
+                riderName.text = riderScore.riderName
+            }
+        }
     }
 
     companion object {
