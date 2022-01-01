e24a30b17c20c31b82ed4c462adde7a823270a3d
Vitali Yakavenka <vitali.yakavenka@equest.com>
2021-12-31 21:53:55 -0800

    Write test file

--- app/src/main/java/net/yakavenka/trialsscore/LeaderboardFragment.kt
+++ app/src/main/java/net/yakavenka/trialsscore/LeaderboardFragment.kt
@@ -1,9 +1,12 @@
 package net.yakavenka.trialsscore
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.DocumentsContract
 import android.util.Log
 import android.view.*
-import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -27,6 +30,9 @@ class LeaderboardFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
+        registerForActivityResult(ActivityResultContracts.CreateDocument()) { uri ->
+            eventScores.exportReport(uri, requireContext().contentResolver)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -51,6 +57,7 @@ class LeaderboardFragment : Fragment() {
         return when (item.itemId) {
             R.id.action_export_results -> {
                 Log.d(TAG, "exporting results")
+                initDownload(Uri.parse("Downloads"))
                 true
             }
             else -> super.onOptionsItemSelected(item)
@@ -78,4 +85,18 @@ class LeaderboardFragment : Fragment() {
             findNavController().navigate(action)
         }
     }
+
+    private fun initDownload(pickerInitialUri: Uri) {
+        val intent = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TITLE, "report.txt")
+
+            // Optionally, specify a URI for the directory that should be opened in
+            // the system file picker before your app creates the document.
+            putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri)
+        }
+        startActivity(intent)
+    }
+
 }
--- app/src/main/java/net/yakavenka/trialsscore/viewmodel/EventScoreViewModel.kt
+++ app/src/main/java/net/yakavenka/trialsscore/viewmodel/EventScoreViewModel.kt
@@ -1,5 +1,7 @@
 package net.yakavenka.trialsscore.viewmodel
 
+import android.content.ContentResolver
+import android.net.Uri
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -7,12 +9,32 @@ import androidx.lifecycle.asLiveData
 import net.yakavenka.trialsscore.data.RiderScoreDao
 import net.yakavenka.trialsscore.data.RiderScoreSummary
 import net.yakavenka.trialsscore.data.ScoreSummaryRepository
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
 import java.lang.IllegalArgumentException
 
 class EventScoreViewModel(
     scoreSummaryRepository: ScoreSummaryRepository
 ) : ViewModel() {
     val allScores: LiveData<List<RiderScoreSummary>> = scoreSummaryRepository.fetchSummary().asLiveData()
+
+    fun exportReport(uri: Uri, contentResolver: ContentResolver) {
+        try {
+            contentResolver.openFileDescriptor(uri, "w")?.use {
+                FileOutputStream(it.fileDescriptor).use {
+                    it.write(
+                        ("Overwritten at ${System.currentTimeMillis()}\n")
+                            .toByteArray()
+                    )
+                }
+            }
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
 
     class Factory(private val riderScoreDao: RiderScoreDao) : ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
