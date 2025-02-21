562b2b638d34c3f7734bee713cc25085e7bfe7e5
Vitali Yakavenka <vitali.yakavenka@equest.com>
2025-02-20 19:55:34 -0800

    Capitalize words when entering rider name

--- app/src/main/java/net/yakavenka/trialsscore/components/EditRiderScreen.kt
+++ app/src/main/java/net/yakavenka/trialsscore/components/EditRiderScreen.kt
@@ -3,6 +3,7 @@ package net.yakavenka.trialsscore.components
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -17,6 +18,7 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
@@ -45,7 +47,8 @@ fun EditRiderScreen(
             singleLine = true,
             modifier = Modifier
                 .padding(8.dp)
-                .fillMaxWidth()
+                .fillMaxWidth(),
+            keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Words)
         )
 
         // We want to react on tap/press on TextField to show menu
