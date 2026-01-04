06946271c1321d07138a7d4a9ef163b2db73026f
Vitali Yakavenka <vitali.yakavenka@equest.com>
2026-01-03 18:16:56 -0800

    Resolve deprecation warnings

--- app/src/main/java/net/yakavenka/trialsscore/components/Sandbox.kt
+++ app/src/main/java/net/yakavenka/trialsscore/components/Sandbox.kt
@@ -3,9 +3,9 @@ package net.yakavenka.trialsscore.components
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -30,7 +30,7 @@ fun Sandbox() {
     ) {
         TextField(
             // The `menuAnchor` modifier must be passed to the text field for correctness.
-            modifier = Modifier.menuAnchor(MenuAnchorType.PrimaryNotEditable, true),
+            modifier = Modifier.menuAnchor(ExposedDropdownMenuAnchorType.PrimaryNotEditable, true),
             readOnly = true,
             value = selectedOptionText,
             onValueChange = {},
