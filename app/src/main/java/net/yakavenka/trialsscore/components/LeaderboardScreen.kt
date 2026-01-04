25153010cec1e45f634b31287a611236301cd2c2
Vitali Yakavenka <vitali.yakavenka@equest.com>
2026-01-03 16:10:46 -0800

    Remove unused imports

--- app/src/main/java/net/yakavenka/trialsscore/components/LeaderboardScreen.kt
+++ app/src/main/java/net/yakavenka/trialsscore/components/LeaderboardScreen.kt
@@ -36,7 +36,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
@@ -44,7 +43,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
